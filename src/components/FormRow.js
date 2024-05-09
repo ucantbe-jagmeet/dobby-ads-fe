207ca48d74eae2a527143df0d4fba646e2fe8262
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-05-10 01:32:43 +0530

    register page done

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -8,18 +8,18 @@ const FormRow = ({
     labelText,
 }) => {
     return (
-        <div className="form-row">
-            <label htmlFor="name" className="form-label">
-                {labelText || name}
-            </label>
-            <input
-                type={type}
-                name={name}
-                value={value}
-                onChange={handleChange}
-                className="form-input"
-            />
-        </div>
+          <div className=" flex flex-col w-full  sm:text-sm text-xs">
+      <label htmlFor="name" className="text-gray-700 capitalize tracking-wide">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        className="border-[0.5px] rounded border-gray-300 mt-1 p-1 outline-none text-gray-600"
+        onChange={handleChange}
+      />
+    </div>
     );
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import { useState } from 'react'
+import FormRow from '../components/FormRow'
 
 const Register = () => {
+    const [values, setValues] = useState({
+        name: '',
+        username: '',
+        password: '',
+        isMember: true,
+    });
+
+    const [isLoading, setIsLoading] = useState(false)
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setValues({ ...values, [name]: value });
+    };
+    const toggleMember = () => {
+        setValues({ ...values, isMember: !values.isMember });
+    };
+
+    const onSubmit = (e) => {
+    };
     return (
-        <div className='bg-red-500'>
-            Register page
-        </div>
+        <main className='h-screen w-screen bg-blue-200 flex items-center justify-center'>
+            <form className='h-3/4 w-1/3 bg-white  rounded shadow px-5 flex flex-col gap-y-6 pt-16' onSubmit={onSubmit}>
+                <h3 className='text-6xl '>
+                    {values.isMember ? "Login" : "Register"}
+                </h3>
+                {
+                    !values.isMember && <FormRow
+                        type="text"
+                        name="name"
+                        value={values.name}
+                        labelText="Name"
+                        handleChange={handleChange}
+                    />
+                }
+                <FormRow
+                    type="text"
+                    name="username"
+                    value={values.username}
+                    labelText="Username"
+                    handleChange={handleChange}
+                />
+                <FormRow
+                    type="password"
+                    name="password"
+                    value={values.password}
+                    labelText="Password"
+                    handleChange={handleChange}
+                />
+                <button type="submit" className={`capitalize px-7 mt-2 py-2 tracking-wider rounded-md bg-[#4939FF] text-white w-full text-lg ${isLoading
+                    ? "bg-gray-400 cursor-not-allowed"
+                    : "bg-[#4939FF] hover:bg-blue-600"
+                    }`} disabled={isLoading}>
+                    {isLoading ? "loading..." : "Submit"}
+                </button>
+
+                <p >
+                    {values.isMember ? "Not a member yet ?" : " Already a member ?"}
+                    <button type="button" className="font-semibold ml-7 bg-green-700 px-2 rounded text-white" onClick={toggleMember}>
+                        {values.isMember ? "Register" : "Login"}
+                    </button>
+                </p>
+            </form>
+        </main>
     )
 }
 
